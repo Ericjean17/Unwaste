1e0f8d8c7a9266d19de3b0ea8e1d0a581c16e8e6
Ericjean17 <eric.jean1864@gmail.com>
2025-08-22 18:00:53 -0400

    bruh

--- server/db.js
+++ server/db.js
@@ -1,21 +1,21 @@
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//     connectionString: process.env.POSTGRES_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-
-// module.exports = pool;
-
 const pool = new Pool({
-    user: "postgres",
-    password: "postgrespw",
-    host: "localhost",
-    port: 5432,
-    database: "unwaste"
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-
 module.exports = pool;
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "postgrespw",
+//     host: "localhost",
+//     port: 5432,
+//     database: "unwaste"
+// });
+
+
+// module.exports = pool;
