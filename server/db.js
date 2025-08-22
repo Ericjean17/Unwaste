471c44a0af08279c2e5627c00e02d04c37897b13
Ericjean17 <eric.jean1864@gmail.com>
2025-08-22 17:26:13 -0400

    db line change

--- server/db.js
+++ server/db.js
@@ -1,10 +1,21 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+// module.exports = pool;
+
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    user: "postgres",
+    password: "postgrespw",
+    host: "localhost",
+    port: 5432,
+    database: "unwaste"
 });
+
 
 module.exports = pool;
