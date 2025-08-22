c2b44b971395569076de1541ca7d9bbe80c5a47a
Ericjean17 <eric.jean1864@gmail.com>
2025-08-22 17:51:36 -0400

    analytics doesnt work

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   // Strict mode is for development, alerts appear twice
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>
 )
