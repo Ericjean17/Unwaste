e75fe3ad0179825ea1c2acdd2e3ea372f75c16c4
Ericjean17 <eric.jean1864@gmail.com>
2025-08-22 02:48:59 -0400

    try 10

--- server/server.js
+++ server/server.js
@@ -7,16 +7,14 @@ const pool = require("./db").default;
 const jwt = require("jsonwebtoken");
 
 // middleware
-// app.use(cors({ origin: 'https://unwaste.vercel.app' }));
+app.use(cors({ origin: 'https://unwaste.vercel.app' }));
 // app.use(cors({ origin: `${process.env.FRONTEND_URL}` }));
-const corsOptions = {
-  origin: ["https://unwaste.vercel.app"],  // allow your frontend
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-};
-app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(express.json()); // allows json data to be put into req.body
 
 function authenticateToken(req, res, next) {
